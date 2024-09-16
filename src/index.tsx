bafebf93fefea393eacefa59b5199799e17ae385
niko <57009359+hauntii@users.noreply.github.com>
2024-09-15 21:29:29 -0400

    Use common Dispatcher

--- src/index.tsx
+++ src/index.tsx
@@ -1,32 +1,29 @@
 /* Created by Hauntii under the GNU GENERAL PUBLIC LICENSE. Do not remove this line. */
+// @ts-nocheck
+
 import manifest from '../manifest.json';
 
 import { Plugin, registerPlugin } from 'enmity/managers/plugins';
 import { bulk, filters } from 'enmity/metro';
 
 import Settings from './components/Settings';
-import { React } from 'enmity/metro/common';
+import { Dispatcher, React } from 'enmity/metro/common';
 import { getBoolean } from 'enmity/api/settings';
 
 const [
    FolderUtils,
-   UserSettingsProtoStore,
-   FluxDispatcher
+   UserSettingsProtoStore
 ] = bulk(
    filters.byProps("isFolderExpanded"),
-   filters.byProps("settings", "_dispatchToken"),
-   filters.byProps(
-      "_currentDispatchActionType",
-      "_subscriptions",
-      "_waitQueue"
-   )
+   filters.byProps("settings", "_dispatchToken")
 );
 
 let collapseFolderEvent = ({ folderId: thisId }) => {
    if (!FolderUtils.isFolderExpanded(thisId)) return;
    UserSettingsProtoStore.settings.guildFolders.folders.filter(f => f.id && FolderUtils.isFolderExpanded(Number(f.id.value))).forEach((folder) => {
       if (Number(folder.id.value) == thisId) return;
-      FluxDispatcher.dispatch({ type: 'TOGGLE_GUILD_FOLDER_EXPAND', folderId: Number(folder.id.value) });
+      // @ts-ignore
+      Dispatcher.dispatch({ type: 'TOGGLE_GUILD_FOLDER_EXPAND', folderId: Number(folder.id.value) });
    });
 };
 
@@ -34,7 +31,8 @@ let channelSelectEvent = ({ guildId: thisId }) => {
    if (getBoolean('FolderCollapse', 'collapseOnDM', false)) {
       if (thisId === null) {
          UserSettingsProtoStore.settings.guildFolders.folders.filter(f => f.id && FolderUtils.isFolderExpanded(Number(f.id.value))).forEach((folder) => {
-            FluxDispatcher.dispatch({ type: 'TOGGLE_GUILD_FOLDER_EXPAND', folderId: Number(folder.id.value) });
+            // @ts-ignore
+            Dispatcher.dispatch({ type: 'TOGGLE_GUILD_FOLDER_EXPAND', folderId: Number(folder.id.value) });
          });
       }
    }
@@ -44,13 +42,13 @@ const FolderCollapse: Plugin = {
    ...manifest,
 
    onStart() {
-      FluxDispatcher.subscribe('TOGGLE_GUILD_FOLDER_EXPAND', collapseFolderEvent);
-      FluxDispatcher.subscribe('CHANNEL_SELECT', channelSelectEvent);
+      Dispatcher.subscribe('TOGGLE_GUILD_FOLDER_EXPAND', collapseFolderEvent);
+      Dispatcher.subscribe('CHANNEL_SELECT', channelSelectEvent);
    },
 
    onStop() {
-      FluxDispatcher.unsubscribe('TOGGLE_GUILD_FOLDER_EXPAND', collapseFolderEvent);
-      FluxDispatcher.unsubscribe('CHANNEL_SELECT', channelSelectEvent);
+      Dispatcher.unsubscribe('TOGGLE_GUILD_FOLDER_EXPAND', collapseFolderEvent);
+      Dispatcher.unsubscribe('CHANNEL_SELECT', channelSelectEvent);
    },
 
    getSettingsPanel({ settings }) {
